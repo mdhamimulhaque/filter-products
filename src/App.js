58bc0a47d3307b325f65deed9cb018fab73c28a8
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2021-11-20 15:47:42 +0600

    edit for dynamic nav items

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 import Menu from "./data";
 import MenuItems from './MenuItems';
 
+//----- start function for dynamic filter navbar
+const allNavItems = ["all",...new Set(Menu.map(data => data.category))];
+//----- end function for dynamic filter navbar
+
 function App() {
   const [ items,setItems ] = useState(Menu);
-
+  const [ navbar, setNavbar ] = useState(allNavItems); //----- use for dynamic filter navbar
+  
   const filterItems = (clickItem) =>{
+    //-----start condition use for dynamic navbar all button
+    if(clickItem === "all"){
+      setItems(Menu);
+      return;
+    }//-----end condition use for dynamic navbar all button
+
      const finalProduct = Menu.filter(pd => pd.category === clickItem);
      setItems(finalProduct);
   }
@@ -19,17 +30,29 @@ function App() {
       <main>
          <nav>
            <center>
-           <button onClick={()=>{setItems(Menu)}}>All</button>
+            {/* ======== this is use for hardCoded Navbar =============== */}
+
+           {/* <button onClick={()=>{setItems(Menu)}}>All</button>
            <button onClick={()=>{filterItems("shakes")}}>Shakes</button>
            <button onClick={()=>{filterItems("breakfast")}}>Breakfast</button>
-           <button onClick={()=>{filterItems("lunch")}}>Lunch</button>
+           <button onClick={()=>{filterItems("lunch")}}>Lunch</button> */}
+
+           {/* ======== this is use for dynamic Navbar =============== */}
+
+           {
+             navbar.map((nav,index) =>{
+              return(
+                <button key={index} onClick={()=>{filterItems(nav)}}>{nav}</button>
+              )
+             })
+           }
            </center>
          </nav>
          <section className="menu-items">
               {
                 items.map(menuItems =>{
                   return(
-                    <MenuItems menuItems={menuItems} />
+                    <MenuItems key={menuItems.id} menuItems={menuItems} />
                   )
                 })
               }
